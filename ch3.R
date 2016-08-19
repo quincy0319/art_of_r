77c693925aca1cc23d7ce25540cfd6bcd19a22a6
quincy0319 <quincy0319@gmail.com>
2016-08-19 19:21:12 +0800

    0819

--- ch3.R
+++ ch3.R
@@ -19,3 +19,45 @@ makecov <- function(rho, n){
         m <- ifelse(row(m) == col(m), 1, rho)  # 对角线元素为1，其余为rho
         return(m)
 }
+
+# 3.3.1
+z <- matrix(1 : 6, nrow = 3, ncol = 2)
+f <- function(x) x / c(2, 8)
+y <- apply(z, 1, f)
+w <- t(y) # 转置
+
+# copymaj
+function(rw, d){
+        maj <- sum(rw[1 : d]) / d
+        return(if(maj > 0.5) 1 else 0)
+}
+
+# 3.4.2 
+# returns the minimum value of d[i, j], i != j
+mind <- function(d){
+        n <- nrow(d)
+        # add a column to identify row number for apply()
+        dd <- cbind(d, 1:n)
+        wmins <- apply(dd[-n, ], 1, imin)
+        # wins will be 2xn, 1st row being indices and 2nd being values
+        i <- which.min(wmins[2, ])
+        j <- wmins[1, i]
+        return(c(d[i, j], i, j))
+}
+
+# finds the location, value of the minimum in a row x
+imin <- function(x){
+        lx <- length(x)
+        i <- x[lx] # original row number
+        j <- which.min(x[(i + 1):(lx - 1)])
+        x <- i + j
+        return(c(k, x[k]))
+}
+
+# if minimum distance between 2 cities is the only one
+# solution below is simpler
+minda <- function(d){
+        smallest <- min(d)
+        ij <- which(d == smallest, arr.ind = TRUE)
+        return(c(smallest, ij))
+}
